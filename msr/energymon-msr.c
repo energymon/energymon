15f49913ffe6fe75d57f5a55b6c3e426f7874e27
Connor Imes <connor.k.imes@gmail.com>
2016-12-05 09:44:56 -0600

    Remove unused MSR preprocessor definitions

--- msr/energymon-msr.c
+++ msr/energymon-msr.c
@@ -30,43 +30,17 @@ int energymon_get_default(energymon* em) {
 
 #define MSR_RAPL_POWER_UNIT		0x606
 
-/*
- * Platform specific RAPL Domains.
- * Note that PP1 RAPL Domain is supported on 062A only
- * And DRAM RAPL Domain is supported on 062D only
- */
 /* Package RAPL Domain */
-#define MSR_PKG_RAPL_POWER_LIMIT	0x610
 #define MSR_PKG_ENERGY_STATUS		0x611
-#define MSR_PKG_PERF_STATUS		0x613
-#define MSR_PKG_POWER_INFO		0x614
 
 /* PP0 RAPL Domain */
-#define MSR_PP0_POWER_LIMIT		0x638
 #define MSR_PP0_ENERGY_STATUS		0x639
-#define MSR_PP0_POLICY			0x63A
-#define MSR_PP0_PERF_STATUS		0x63B
 
 /* PP1 RAPL Domain, may reflect to uncore devices */
-#define MSR_PP1_POWER_LIMIT		0x640
 #define MSR_PP1_ENERGY_STATUS		0x641
-#define MSR_PP1_POLICY			0x642
 
 /* DRAM RAPL Domain */
-#define MSR_DRAM_POWER_LIMIT		0x618
 #define MSR_DRAM_ENERGY_STATUS		0x619
-#define MSR_DRAM_PERF_STATUS		0x61B
-#define MSR_DRAM_POWER_INFO		0x61C
-
-/* RAPL UNIT BITMASK */
-#define POWER_UNIT_OFFSET	0
-#define POWER_UNIT_MASK		0x0F
-
-#define ENERGY_UNIT_OFFSET	0x08
-#define ENERGY_UNIT_MASK	0x1F00
-
-#define TIME_UNIT_OFFSET	0x10
-#define TIME_UNIT_MASK		0xF000
 
 typedef struct msr_info {
   int fd;
